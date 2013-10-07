c36a2119eca45480b3cf0782829c9755177dd910
Devian Simmons <devians80@gmail.com>
2013-10-07 14:40:35 -0500

    Added background color to each fragment incorparated list array to tabs 1-2

---
+++ FragmentTabsTutorial/src/main/java/com/threedigital/fragmenttabstutorial/FragmentTab4.java
@@ -0,0 +1,19 @@
+package com.threedigital.fragmenttabstutorial;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by 3digital on 10/7/13.
+ */
+public class FragmentTab4 extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragmenttab4, container, false);
+    }
+}
--- FragmentTabsTutorial/src/main/java/com/threedigital/fragmenttabstutorial/MainActivity.java
+++ FragmentTabsTutorial/src/main/java/com/threedigital/fragmenttabstutorial/MainActivity.java
@@ -14,18 +14,19 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
 
-        ActionBar.Tab Tab1, Tab2, Tab3;
+        ActionBar.Tab Tab1, Tab2, Tab3, Tab4;
         Fragment fragmentTab1 = new FragmentTab1();
         Fragment fragmentTab2 = new FragmentTab2();
         Fragment fragmentTab3 = new FragmentTab3();
+        Fragment fragmentTab4 = new FragmentTab4();
 
 
 
             ActionBar actionBar = getActionBar();
 
             // Hide Actionbar Icon
-        assert actionBar != null;
-        actionBar.setDisplayShowHomeEnabled(false);
+            assert actionBar != null;
+            actionBar.setDisplayShowHomeEnabled(false);
 
             // Hide Actionbar Title
             actionBar.setDisplayShowTitleEnabled(false);
@@ -37,16 +38,19 @@ public class MainActivity extends Activity {
             Tab1 = actionBar.newTab().setText("Tab1");
             Tab2 = actionBar.newTab().setText("Tab2");
             Tab3 = actionBar.newTab().setText("Tab3");
+            Tab4 = actionBar.newTab().setText("Tab4");
 
             // Set Tab Listeners
             Tab1.setTabListener(new TabListener(fragmentTab1));
             Tab2.setTabListener(new TabListener(fragmentTab2));
             Tab3.setTabListener(new TabListener(fragmentTab3));
+            Tab4.setTabListener(new TabListener(fragmentTab4));
 
             // Add tabs to actionbar
             actionBar.addTab(Tab1);
             actionBar.addTab(Tab2);
             actionBar.addTab(Tab3);
+            actionBar.addTab(Tab4);
         }
     }
 
